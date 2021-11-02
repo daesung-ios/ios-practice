47951e116d2c52d90d055a385249c48962db6201
이대승 <daesung.lee@peoplefund.co.kr>
2021-11-02 14:56:04 +0900

    first commit

---
+++ ios-practice/ios-practice/ios_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_practiceApp.swift
+//  ios-practice
+//
+//  Created by temp on 2021/11/02.
+//
+
+import SwiftUI
+
+@main
+struct ios_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
